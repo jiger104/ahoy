7ed3a358d65297499adae42971b4c7c9fab3ad77
Jigar <jigar@automationzen.com>
2022-12-08 15:08:37 -0600

    add confetti and img for denied application

--- src/routes/bank-application/ApplicationResult.svelte
+++ src/routes/bank-application/ApplicationResult.svelte
@@ -1,8 +1,11 @@
 <script>
 	export let applicationResult;
+	import { Confetti } from 'svelte-confetti';
 </script>
 
 {#if applicationResult.outcome === 'Approved'}
+	<Confetti />
+
 	<h2>
 		Congratulations {applicationResult.data.firstName}, your application has been approved! 🎉🎉🎉
 	</h2>
@@ -11,5 +14,8 @@
 		Thanks for submitting your application {applicationResult.data.firstName} , we’ll be in touch shortly
 	</h2>
 {:else if applicationResult.outcome === 'Denied'}
-	<h2>Sorry {applicationResult.data.firstName} your application was not successfull</h2>
+	<div class="flex flex-col items-center justify-center gap-y-6">
+		<img width="100" height="100" src="sad-face.svg" />
+		<h2>Sorry {applicationResult.data.firstName} your application was not successfull</h2>
+	</div>
 {/if}
